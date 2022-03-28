3166507a75f691ff8af5344dea36ede1185a15b9
Vadyxa715 <vadim_kurdin@mail.ru>
2022-03-28 17:27:04 +0300

    pawn logic move

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -10,10 +10,17 @@ public abstract class Piece {
 
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
     Piece(final int piecePosition, final Alliance pieceAlliance) {
         this.pieceAlliance = pieceAlliance;
         this.piecePosition = piecePosition;
+        //todo more work here
+        this.isFirstMove = false;
+    }
+
+    public boolean isFirstMove() {
+        return this.isFirstMove;
     }
 
     public Alliance getPieceAlliance() {
